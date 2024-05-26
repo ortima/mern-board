f5a05c2f8a0d89c9ab2dea167b7d33f2ff084d0e
ortima <account.net@.mail.ru>
2024-05-26 23:53:28 +0300

    fix error

--- client/src/utils/toggleTheme.tsx
+++ client/src/utils/toggleTheme.tsx
@@ -6,53 +6,53 @@ import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
 import LightModeIcon from '@mui/icons-material/LightMode';
 
 export default function ColorSchemeToggle(props: IconButtonProps) {
-   const { onClick, sx, ...other } = props;
-   const { mode, setMode } = useColorScheme();
-   const [mounted, setMounted] = React.useState(false);
-   React.useEffect(() => {
-      setMounted(true);
-   }, []);
-   if (!mounted) {
-      return (
-         <IconButton
-            size="sm"
-            variant="outlined"
-            color="neutral"
-            {...other}
-            sx={sx}
-            disabled
-         />
-      );
-   }
-   return (
+  const { onClick, sx, ...other } = props;
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return (
       <IconButton
-         id="toggle-mode"
-         size="sm"
-         variant="outlined"
-         color="neutral"
-         {...other}
-         onClick={(event) => {
-            if (mode === 'light') {
-               setMode('dark');
-            } else {
-               setMode('light');
-            }
-            onClick?.(event);
-         }}
-         sx={[
-            {
-               '& > *:first-child': {
-                  display: mode === 'dark' ? 'none' : 'initial',
-               },
-               '& > *:last-child': {
-                  display: mode === 'light' ? 'none' : 'initial',
-               },
-            },
-            ...(Array.isArray(sx) ? sx : [sx]),
-         ]}
-      >
-         <DarkModeRoundedIcon />
-         <LightModeIcon />
-      </IconButton>
-   );
+        size="sm"
+        variant="outlined"
+        color="neutral"
+        {...other}
+        sx={sx}
+        disabled
+      />
+    );
+  }
+  return (
+    <IconButton
+      id="toggle-mode"
+      size="sm"
+      variant="outlined"
+      color="neutral"
+      {...other}
+      onClick={(event) => {
+        if (mode === 'light') {
+          setMode('dark');
+        } else {
+          setMode('light');
+        }
+        onClick?.(event);
+      }}
+      sx={[
+        {
+          '& > *:first-of-type': {
+            display: mode === 'dark' ? 'none' : 'initial',
+          },
+          '& > *:last-child': {
+            display: mode === 'light' ? 'none' : 'initial',
+          },
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    >
+      <DarkModeRoundedIcon />
+      <LightModeIcon />
+    </IconButton>
+  );
 }
