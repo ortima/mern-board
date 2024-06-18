e13d9c982fa68c413e2c8a2a283751f837bb0d0a
ortima <account.net@mail.ru>
2024-06-18 23:18:51 +0300

    fixes

--- client/src/components/Appbar.tsx
+++ client/src/components/Appbar.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState } from 'react'
 import { styled } from '@mui/material/styles'
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
@@ -7,6 +8,7 @@ import IconButton from '@mui/material/IconButton'
 import Badge from '@mui/material/Badge'
 import MenuIcon from '@mui/icons-material/Menu'
 import NotificationsIcon from '@mui/icons-material/Notifications'
+import { Avatar, Box, Menu, MenuItem, Tooltip } from '@mui/material'
 
 interface AppBarProps extends MuiAppBarProps {
   open?: boolean
@@ -15,7 +17,7 @@ interface AppBarProps extends MuiAppBarProps {
 
 const drawerWidth = 240
 
-const AppBar = styled(MuiAppBar, {
+const StyledAppBar = styled(MuiAppBar, {
   shouldForwardProp: prop => prop !== 'open',
 })<AppBarProps>(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
@@ -33,35 +35,79 @@ const AppBar = styled(MuiAppBar, {
   }),
 }))
 
-const AppBarComponent: React.FC<AppBarProps> = ({ open, toggleDrawer }) => (
-  <AppBar toggleDrawer={toggleDrawer} position="absolute" open={open}>
-    <Toolbar sx={{ pr: '24px' }}>
-      <IconButton
-        edge="start"
-        color="inherit"
-        aria-label="open drawer"
-        onClick={toggleDrawer}
-        sx={{
-          marginRight: '36px',
-          ...(open && { display: 'none' }),
-        }}>
-        <MenuIcon />
-      </IconButton>
-      <Typography
-        component="h1"
-        variant="h6"
-        color="inherit"
-        noWrap
-        sx={{ flexGrow: 1 }}>
-        Dashboard
-      </Typography>
-      <IconButton color="inherit">
-        <Badge badgeContent={4} color="secondary">
-          <NotificationsIcon />
-        </Badge>
-      </IconButton>
-    </Toolbar>
-  </AppBar>
-)
+const AppBarComponent: React.FC<AppBarProps> = ({ open, toggleDrawer }) => {
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
+  const settings = ['Setting 1', 'Setting 2', 'Setting 3']
+
+  return (
+    <StyledAppBar toggleDrawer={toggleDrawer} position="absolute" open={open}>
+      <Toolbar sx={{ pr: '24px' }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={toggleDrawer}
+          sx={{
+            marginRight: '36px',
+            ...(open && { display: 'none' }),
+          }}>
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          sx={{ flexGrow: 1 }}>
+          Dashboard
+        </Typography>
+        <IconButton color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="User Avatar" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}>
+            {settings.map(setting => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography variant="body1" align="center">
+                  {setting}
+                </Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </StyledAppBar>
+  )
+}
 
 export default AppBarComponent
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,11 +1,15 @@
+import * as React from 'react'
 import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import Link from '@mui/material/Link'
+import Button from '@mui/material/Button'
 import Layout from '../components/Layout'
+import TableComponent from '../components/dashboard/Table'
+import TransactionModal from '../components/dashboard/AddModal'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store'
 
 function Copyright(props: any) {
   return (
@@ -25,6 +29,27 @@ function Copyright(props: any) {
 }
 
 export default function Dashboard() {
+  const { transactions, loading, error } = useSelector(
+    (state: RootState) => state.transactions,
+  )
+  const [modalOpen, setModalOpen] = React.useState(false)
+
+  const handleModalOpen = () => {
+    setModalOpen(true)
+  }
+
+  const handleModalClose = () => {
+    setModalOpen(false)
+  }
+
+  const handleTransactionSubmit = (transaction: {
+    name: string
+    amount: number
+  }) => {
+    console.log('New Transaction:', transaction)
+    // Add logic to handle the new transaction
+  }
+
   return (
     <Layout>
       <Box sx={{ display: 'flex' }}>
@@ -39,39 +64,33 @@ export default function Dashboard() {
             height: '100vh',
             overflow: 'auto',
           }}>
-          <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}></Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}></Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                  }}></Paper>
-              </Grid>
-            </Grid>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Typography variant="h6" gutterBottom>
+                Transactions
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleModalOpen}>
+                Add Transaction
+              </Button>
+              <TransactionModal
+                open={modalOpen}
+                handleClose={handleModalClose}
+                handleSubmit={handleTransactionSubmit}
+              />
+            </Box>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 'auto',
+                mt: 2,
+              }}>
+              <TableComponent transactions={transactions} />
+            </Paper>
             <Copyright sx={{ pt: 4 }} />
           </Container>
         </Box>
