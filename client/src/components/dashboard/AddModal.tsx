f41f7b21fdd9c39c2a9fb58bb9f65e36d0de5e72
Timur <orakov.fortech@gmail.com>
2024-06-19 14:28:10 +0300

    feature: add modal to add transactions

--- client/src/components/dashboard/AddModal.tsx
+++ client/src/components/dashboard/AddModal.tsx
@@ -1,66 +1,118 @@
-import * as React from 'react'
-import Button from '@mui/material/Button'
-import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
-import TextField from '@mui/material/TextField'
+import * as React from "react"
+import Button from "@mui/material/Button"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import Input from "@mui/material/Input"
+import Dialog from "@mui/material/Dialog"
+import DialogTitle from "@mui/material/DialogTitle"
+import Stack from "@mui/material/Stack"
+import AddIcon from "@mui/icons-material/Add"
+import { useDispatch } from "react-redux"
+import { addTransaction } from "../../store/transactionSlice"
+import { AppDispatch } from "../../store"
+import { NumericFormatAdapter } from "../../utils/numericFormat"
+import CustomSelectFormControl from "../shared/CustomSelectFormControl"
 
-interface TransactionModalProps {
-  open: boolean
-  handleClose: () => void
-  handleSubmit: (transaction: { name: string; amount: number }) => void
+interface FormElements extends HTMLFormControlsCollection {
+  type: HTMLSelectElement | HTMLInputElement | any
+  category: HTMLSelectElement
+  description: HTMLInputElement
+  amount: HTMLInputElement
 }
 
-const TransactionModal: React.FC<TransactionModalProps> = ({
-  open,
-  handleClose,
-  handleSubmit,
-}) => {
-  const [name, setName] = React.useState('')
-  const [amount, setAmount] = React.useState<number | string>('')
+interface Form extends HTMLFormElement {
+  readonly elements: FormElements
+}
 
-  const onSubmit = () => {
-    handleSubmit({ name, amount: Number(amount) })
-    handleClose()
+export default function BasicModalDialog() {
+  const dispatch: AppDispatch = useDispatch()
+  const [open, setOpen] = React.useState<boolean>(false)
+
+  async function submitHandler(event: React.FormEvent<Form>) {
+    event.preventDefault()
+    const userDataString = localStorage.getItem("userData")
+    const userData = userDataString ? JSON.parse(userDataString) : null
+    const userId = userData ? userData.userId : null
+
+    if (!userId) {
+      console.error("userId not found in localStorage")
+      return
+    }
+
+    const formData = new FormData(event.currentTarget)
+    const formJson = Object.fromEntries((formData as any).entries())
+    const data: any = {
+      userId: userId,
+      type: formJson.type,
+      category: formJson.category,
+      description: formJson.description,
+      amount: formJson.amount,
+    }
+
+    console.log(data)
+
+    try {
+      const response = await dispatch(addTransaction(data))
+      if (response.meta.requestStatus === "fulfilled") {
+        setOpen(false)
+      } else {
+        console.error("Failed to add transaction")
+      }
+    } catch (error) {
+      console.error(error, "Error")
+    }
   }
 
   return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Create Transaction</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          To create a new transaction, please enter the transaction name and
-          amount here.
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Transaction Name"
-          fullWidth
-          variant="outlined"
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-        <TextField
-          margin="dense"
-          label="Amount"
-          type="number"
-          fullWidth
-          variant="outlined"
-          value={amount}
-          onChange={e => setAmount(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={onSubmit} color="primary">
-          Create
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <React.Fragment>
+      <Button
+        variant="outlined"
+        startIcon={<AddIcon />}
+        onClick={() => setOpen(true)}
+      >
+        New transaction
+      </Button>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Create new transaction</DialogTitle>
+        <form onSubmit={submitHandler}>
+          <Stack spacing={2} sx={{ padding: 2 }}>
+            <CustomSelectFormControl
+              label="Type"
+              defaultValue="expense"
+              name="type"
+              options={[
+                { value: "expense", label: "Расходы" },
+                { value: "income", label: "Доходы" },
+              ]}
+            />
+            <CustomSelectFormControl
+              label="Category"
+              name="category"
+              defaultValue="school"
+              options={[
+                { value: "school", label: "Школа" },
+                { value: "work", label: "Работа" },
+                { value: "university", label: "Университет" },
+              ]}
+            />
+            <FormControl fullWidth>
+              <InputLabel>Description</InputLabel>
+              <Input required name="description" />
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel>Amount</InputLabel>
+              <Input
+                name="amount"
+                required
+                inputComponent={NumericFormatAdapter as any}
+              />
+            </FormControl>
+            <Button type="submit" variant="contained">
+              Submit
+            </Button>
+          </Stack>
+        </form>
+      </Dialog>
+    </React.Fragment>
   )
 }
-
-export default TransactionModal
