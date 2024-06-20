2a7d88929ab7796dd80c3caf1a795655fed6c313
Timur <orakov.fortech@gmail.com>
2024-06-20 11:48:15 +0300

    feat: add prop for modal

--- client/src/components/shared/CustomSelectFormControl.tsx
+++ client/src/components/shared/CustomSelectFormControl.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { ChangeEvent } from "react"
 import FormControl from "@mui/material/FormControl"
 import InputLabel from "@mui/material/InputLabel"
-import Select from "@mui/material/Select"
+import Select, { SelectChangeEvent } from "@mui/material/Select"
 import MenuItem from "@mui/material/MenuItem"
 
 interface CustomSelectFormControlProps {
@@ -9,6 +9,7 @@ interface CustomSelectFormControlProps {
   name: string
   defaultValue: string
   options: Array<{ value: string; label: string }>
+  onChange?: (e: SelectChangeEvent<string>, child: React.ReactNode) => void
 }
 
 const CustomSelectFormControl: React.FC<CustomSelectFormControlProps> = ({
@@ -16,11 +17,12 @@ const CustomSelectFormControl: React.FC<CustomSelectFormControlProps> = ({
   name,
   defaultValue,
   options,
+  onChange,
 }) => {
   return (
     <FormControl fullWidth>
       <InputLabel>{label}</InputLabel>
-      <Select defaultValue={defaultValue} name={name}>
+      <Select defaultValue={defaultValue} name={name} onChange={onChange}>
         {options.map((option) => (
           <MenuItem key={option.value} value={option.value}>
             {option.label}
