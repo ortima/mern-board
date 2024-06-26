806a0f13283fd3c344ac59310d6e05dea4e3d9f5
Timur <orakov.fortech@gmail.com>
2024-06-26 17:42:12 +0300

    feat: added deal

--- client/src/components/shared/CustomDeal.tsx
+++ client/src/components/shared/CustomDeal.tsx
@@ -1,17 +1,52 @@
-import Box from "@mui/material/Box";
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
+import React, { useState } from "react";
+import {
+  Box,
+  SpeedDialAction,
+  SpeedDial,
+  SpeedDialIcon,
+  Typography,
+} from "@mui/material";
+import { UploadFile, FileCopy } from "@mui/icons-material";
+import CustomUploadFileForm from "./AddFileModal";
+import UploadFileForm from "./UploadFileForm";
 
-const actions = [{ icon: <FileCopyIcon />, name: "Download template" }];
+const actions = [
+  { icon: <FileCopy />, name: "Download template", action: "download" },
+  { icon: <UploadFile />, name: "Upload .xl file", action: "upload" },
+];
 
 const CustomDeal = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleActionClick = (action: string) => {
+    if (action === "download") {
+      const link = document.createElement("a");
+      link.href = "/template.ods";
+      link.download = "template.ods";
+      link.click();
+    } else if (action === "upload") {
+      setOpen(true);
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Box sx={{ height: 100, transform: "translateZ(0px)", flexGrow: 1 }}>
+    <Box
+      sx={{
+        position: "absolute",
+        bottom: "10px",
+        right: "20px",
+        height: 100,
+        transform: "translateZ(0px)",
+        flexGrow: 1,
+      }}
+    >
       <SpeedDial
         ariaLabel="SpeedDial basic example"
-        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        sx={{ position: "absolute", bottom: 8, right: 20 }}
         icon={<SpeedDialIcon />}
       >
         {actions.map((action) => (
@@ -19,10 +54,22 @@ const CustomDeal = () => {
             key={action.name}
             icon={action.icon}
             tooltipTitle={action.name}
+            onClick={() => handleActionClick(action.action)}
           />
         ))}
       </SpeedDial>
+
+      <CustomUploadFileForm open={open} onClose={handleClose}>
+        <Box sx={{ width: 400 }}>
+          <Typography variant="h6" component="h2" gutterBottom>
+            Upload .xl file
+          </Typography>
+
+          <UploadFileForm />
+        </Box>
+      </CustomUploadFileForm>
     </Box>
   );
 };
+
 export default CustomDeal;
